67be4aa580ea3c2cc83ca48a71f1c7b92b755ce0
wojciechfpga <lasowski.wojciech.poczta@gmail.com>
2024-12-18 10:13:14 +0100

    First

---
+++ front-end/src/config/apiRoutes.js
@@ -0,0 +1,8 @@
+export const API_ROUTES = {
+    RESERVATIONS: 'http://localhost:5000/api/reservations',
+    MY_RESERVATIONS: 'http://localhost:5000/api/reservations/my',
+    ROOMS: 'http://localhost:5000/api/rooms',
+    LOGIN: 'http://localhost:5000/api/login',
+    REGISTER: 'http://localhost:5000/api/register',
+  };
+  
---
+++ front-end/src/services/apiService.js
@@ -0,0 +1,66 @@
+import axios from 'axios';
+import { API_ROUTES } from '../config/apiRoutes';
+import { ERROR_MESSAGES } from '../config/errorMessages';
+import { ALERT_MESSAGES } from '../config/alertMessages';
+
+export const apiPostRegister = async (userData) => {
+  const response = await axios.post(API_ROUTES.REGISTER, userData);
+  return response.data;
+}
+
+export const apiPostLogin = async (credentials) => {
+  const response = await axios.post(API_ROUTES.LOGIN, credentials);
+  return response.data;
+}
+
+export const apiGetRetriveMyReservations = async (token) => {
+  try {
+    const response = await axios.get(API_ROUTES.MY_RESERVATIONS, {
+      headers: { Authorization: `${token}` },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(ERROR_MESSAGES.NETWORK_ERROR+error);
+  }
+};
+
+export const apiPostSendReservation = async (formData, token) => {
+  try {
+    await axios.post(API_ROUTES.RESERVATIONS, formData, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+    });
+    alert(ALERT_MESSAGES.RESERVATION_CREATED);
+  } catch {
+    alert(ALERT_MESSAGES.RESERVATION_CREATION_ERROR);
+  }
+};
+
+export const apiGetRetrieveRooms = async () => {
+  try {
+    const { data } = await axios.get(API_ROUTES.ROOMS);
+    return data;
+  } catch (error) {
+    throw new Error(ERROR_MESSAGES.FETCH_ROOMS_ERROR+error);
+  }
+};
+
+export const apiPatchUpdateReservation = async (token, event) => {
+  try {
+    const response = await axios.patch(
+      `${API_ROUTES.RESERVATIONS}/${event.id}`,
+      event,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
