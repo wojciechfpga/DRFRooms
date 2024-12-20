50728cf601e1cd430d7ff6fd8529b3610eab2351
wojciechfpga <lasowski.wojciech.poczta@gmail.com>
2024-12-21 00:30:38 +0100

    cors ommited

--- front-end/src/app/roomsadmin/page.jsx
+++ front-end/src/app/roomsadmin/page.jsx
@@ -1,15 +1,26 @@
-'use client'
+'use client';
 
 import React from 'react';
 import RoomListAdmin from '../../components/Room/RoomListAdmin';
 import { useRoomList } from '../../hooks/room/useRoomList';
 
 const RoomsPageAdmin = () => {
-  const { rooms, addRoom, updateRoom, deleteRoom } = useRoomList();
+  const { rooms, isLoading, addRoom, updateRoom, deleteRoom } = useRoomList();
 
   return (
     <div className="min-h-screen bg-gray-200 flex justify-center items-center">
-      <RoomListAdmin rooms={rooms} onAddRoom={addRoom} onUpdateRoom={updateRoom} onDeleteRoom={deleteRoom} />
+      {isLoading ? (
+        <div className="flex justify-center items-center">
+          <div className="animate-spin rounded-full border-t-4 border-b-4 border-blue-500 w-[8vh] h-[8vh]"></div>
+        </div>
+      ) : (
+        <RoomListAdmin
+          rooms={rooms}
+          onAddRoom={addRoom}
+          onUpdateRoom={updateRoom}
+          onDeleteRoom={deleteRoom}
+        />
+      )}
     </div>
   );
 };
--- front-end/src/hooks/room/useRoomList.jsx
+++ front-end/src/hooks/room/useRoomList.jsx
@@ -1,29 +1,57 @@
-'use client'
-
 import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import axios from 'axios';
-import * as fn from './../../services/apiService'
+import * as fn from './../../services/apiService';
+
 export const useRoomList = () => {
   const [rooms, setRooms] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const { token } = useSelector((state) => state.auth);
 
   useEffect(() => {
-    axios.get('/adminapi/rooms').then((response) => setRooms(response.data));
+    const fetchRooms = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get('/adminapi/rooms');
+        setRooms(response.data);
+      } catch (error) {
+        console.error('Error fetching rooms:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchRooms();
   }, []);
 
   const addRoom = async (room) => {
-    const response = await fn.apiAdminPostSendRoom(room,'none')
-    setRooms([...rooms, response.data]);
+    setIsLoading(true);
+    try {
+      const response = await fn.apiAdminPostSendRoom(room, token);
+      setRooms([...rooms, response.data]);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const updateRoom = async (room) => {
-    await fn.apiAdminPutUpdateRoom("none", room)
-    setRooms(rooms.map((r) => (r.id === room.id ? room : r)));
+    setIsLoading(true);
+    try {
+      await fn.apiAdminPutUpdateRoom(token, room);
+      setRooms(rooms.map((r) => (r.id === room.id ? room : r)));
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const deleteRoom = async (id) => {
-    await fn.apiAdminDeleteRoom("none",id)
-    setRooms(rooms.filter((room) => room.id !== id));
+    setIsLoading(true);
+    try {
+      await fn.apiAdminDeleteRoom(token, id);
+      setRooms(rooms.filter((room) => room.id !== id));
+    } finally {
+      setIsLoading(false);
+    }
   };
 
-  return { rooms, addRoom, updateRoom, deleteRoom };
+  return { rooms, isLoading, addRoom, updateRoom, deleteRoom };
 };
