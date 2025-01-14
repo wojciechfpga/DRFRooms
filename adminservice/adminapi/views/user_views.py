63068d694d56062556b65e96a2c053f6ab43b1d8
wojciechfpga <lasowski.wojciech.poczta@gmail.com>
2025-01-14 23:22:23 +0100

    Users

--- adminservice/adminapi/views/user_views.py
+++ adminservice/adminapi/views/user_views.py
@@ -1,15 +1,17 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.filters import SearchFilter
 from ..models import Users
 from ..serializers import UserSerializer
-from rest_framework.pagination import PageNumberPagination
 
-# Paginator dla infinite scroll
-class InfiniteScrollPagination(PageNumberPagination):
-    page_size = 10  # Liczba elementów na stronę
+class UsersPagination(PageNumberPagination):
+    page_size = 10 
     page_size_query_param = 'page_size'
     max_page_size = 100
 
 class UserListView(ListAPIView):
     queryset = Users.objects.all()
     serializer_class = UserSerializer
-    pagination_class = InfiniteScrollPagination
+    pagination_class = UsersPagination
+    filter_backends = [SearchFilter]
+    search_fields = ['username']  
